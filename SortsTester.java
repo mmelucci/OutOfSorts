a1ddfbb9dde2420e69e9214390b71a9ab438061b
mmelucci <mmelucci20@stuy.edu>
2020-12-16 23:47:26 -0500

    Added more tests

--- SortsTester.java
+++ SortsTester.java
@@ -2,6 +2,12 @@ public class SortsTester {
 
 	public static void main(String[] args) {
     int[] test1 = {16,10,9,17,8,5,7,3,1,19};
+    int[] test2 = {};
+    int[] test3 = {1, 1, 1, 2, 1502, 2, 4, 2,1231, 1};
+    int[] test4 = {10, 9, 8, 7, 6, 5, 4, 1};
     Sorts.bubbleSort(test1);
+    Sorts.bubbleSort(test2);
+    Sorts.bubbleSort(test3);
+    Sorts.bubbleSort(test4);
   }
 }
