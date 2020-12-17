82a10e48b13b0dadab3f818d78da0d3875386459
mmelucci <mmelucci20@stuy.edu>
2020-12-16 23:39:51 -0500

    Created the bubbleSort method and made tests. Still ironing out compilation

--- Sorts.java
+++ Sorts.java
@@ -1,8 +1,26 @@
+import java.util.Arrays;
 public class Sorts{
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  // public static void bubbleSort(int[] data){
-  // }
+  public static void bubbleSort(int[] data){
+    int temp = 0;
+    boolean swap;
+    for (int j = data.length-1; j >= 0; j --) {
+      swap = false;
+      System.out.println(Arrays.toString(data));
+      for (int i = 0; i < j; i ++) {
+        if (data[i] > data[i+1]) {
+          temp = data[i];
+          data[i] = data[i+1];
+          data[i+1] = temp;
+          swap = true;
+        }
+      }
+      if (!swap) {
+        break;
+      }
+    }
+  }
 }
---
+++ SortsTester.java
@@ -0,0 +1,7 @@
+public class SortsTester {
+
+	public static void main(String[] args) {
+    int[] test1 = {16,10,9,17,8,5,7,3,1,19};
+    Sorts.bubbleSort(test1);
+  }
+}
