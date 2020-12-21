654583bcf07bf303218f25829a8cf74a9e5bbdb6
mmelucci <mmelucci20@stuy.edu>
2020-12-20 22:17:54 -0500

    Completed more tests

--- SortsTester.java
+++ SortsTester.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class SortsTester {
 
 	public static void main(String[] args) {
@@ -5,15 +6,40 @@ public class SortsTester {
     int[] test2 = {};
     int[] test3 = {1, 1, 1, 2, 1502, 2, 4, 2,1231, 1};
     int[] test4 = {10, 9, 8, 7, 6, 5, 4, 1};
-    // Sorts.bubbleSort(test1);
-    // Sorts.bubbleSort(test2);
-    // Sorts.bubbleSort(test3);
-    // Sorts.bubbleSort(test4);
-		// Sorts.selectionSort(test1);
-		// Sorts.selectionSort(test2);
-		// Sorts.selectionSort(test3);
-		Sorts.insertionSort(test1);
-		Sorts.insertionSort(test2);
-		Sorts.insertionSort(test3);
+		System.out.println("BUBBLE SORT TESTS:");
+    Sorts.bubbleSort(test1);
+    Sorts.bubbleSort(test2);
+    Sorts.bubbleSort(test3);
+    Sorts.bubbleSort(test4);
+		System.out.println(Arrays.toString(test1));
+		System.out.println(Arrays.toString(test2));
+		System.out.println(Arrays.toString(test3));
+		System.out.println(Arrays.toString(test4));
+		int[] test5 = {16,10,9,17,8,5,7,3,1,19};
+  	int[] test6 = {};
+    int[] test7 = {1, 1, 1, 2, 1502, 2, 4, 2,1231, 1};
+		int[] test8 = {10, 9, 8, 7, 6, 5, 4, 1};
+		System.out.println("SELECTION SORT TESTS:");
+		Sorts.selectionSort(test5);
+		Sorts.selectionSort(test6);
+		Sorts.selectionSort(test7);
+		Sorts.selectionSort(test8);
+		System.out.println(Arrays.toString(test5));
+		System.out.println(Arrays.toString(test6));
+		System.out.println(Arrays.toString(test7));
+		System.out.println(Arrays.toString(test8));
+		int[] test9 = {16,10,9,17,8,5,7,3,1,19};
+  	int[] test10 = {};
+    int[] test11 = {1, 1, 1, 2, 1502, 2, 4, 2,1231, 1};
+    int[] test12 = {10, 9, 8, 7, 6, 5, 4, 1};
+		System.out.println("INSERTION SORT TESTS:");
+		Sorts.insertionSort(test9);
+		Sorts.insertionSort(test10);
+		Sorts.insertionSort(test11);
+		Sorts.insertionSort(test12);
+		System.out.println(Arrays.toString(test9));
+		System.out.println(Arrays.toString(test10));
+		System.out.println(Arrays.toString(test11));
+		System.out.println(Arrays.toString(test12));
   }
 }
